8b3a39f4c9b03bc2ad967041ad0ebc9a2aff41da
larson227 <larson227@gmail.com>
2018-02-08 17:20:50 +0200

    Add test edit group modification

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModofictaionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModofictaionTests.java
@@ -3,16 +3,14 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-public class GroupCreationTests extends TestBase {
-
+public class GroupModofictaionTests extends TestBase {
     @Test
-    public void testGroupCreation() {
+    public void testGroupModification(){
         app.getNabigationHelper().gotoGroupPage();
         app.getGroupHelper().selectGroup();
-        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("sdf", "sdf", "sdf"));
-        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
     }
-
 }
